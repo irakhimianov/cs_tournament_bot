9f8a6785b97f6b142be5d6103cc66f4f4056f24d
Ilgiz Rakhimianov <i@rakhimianov.ru>
2025-08-15 19:06:48 +0500

    Edit

--- keyboards/inline/tournament.py
+++ keyboards/inline/tournament.py
@@ -1,5 +1,7 @@
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
+from keyboards.inline.back import get_back_button
+
 
 def get_tournaments(tournaments: dict[int, str]):
     keyboard = InlineKeyboardMarkup(row_width=1)
@@ -8,12 +10,16 @@ def get_tournaments(tournaments: dict[int, str]):
         buttons.append(
             InlineKeyboardButton(text=name, callback_data=f'get_tournament_{id}'),
         )
+    buttons.append(get_back_button('menu'))
     keyboard.add(*buttons)
     return keyboard
 
 
 def get_tournament_keyboard(tournament_id) -> InlineKeyboardMarkup:
     keyboard = InlineKeyboardMarkup(row_width=1)
-    button = InlineKeyboardButton(text='🔥 Участвовать', callback_data=f'join_tournament_{tournament_id}')
-    keyboard.add(button)
+    buttons = [
+        InlineKeyboardButton(text='🔥 Участвовать', callback_data=f'join_tournament_{tournament_id}'),
+        get_back_button('active_tournaments'),
+    ]
+    keyboard.add(*buttons)
     return keyboard
